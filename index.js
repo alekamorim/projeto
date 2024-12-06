bc055b1d498026e0cb43044c55def066350f3f9e
alekamorim <aleckamorim2006@gmail.com>
2024-12-05 21:43:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('aumentar').addEventListener('click', function() {
+    document.body.style.fontSize = '30px';
+});
+
+document.getElementById('diminuir').addEventListener('click', function() {
+    document.body.style.fontSize = '16px';
+});
