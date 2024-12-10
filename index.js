13310d97d20b6848abcb739b725672185f46b786
alekamorim <aleckamorim2006@gmail.com>
2024-12-09 21:31:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-document.getElementById('aumentar').addEventListener('click', function() {
+document.getElementById('btn1').addEventListener('click', function() {
     document.body.style.fontSize = '30px';
 });
 
-document.getElementById('diminuir').addEventListener('click', function() {
+document.getElementById('btn2').addEventListener('click', function() {
     document.body.style.fontSize = '16px';
 });
